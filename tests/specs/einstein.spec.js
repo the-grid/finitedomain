6ae1815dfa947fac564a64070a8735d63100406c
Peter van der Zee <github-public@qfox.nl>
2016-11-02 09:38:00 +0100

    Add DSL version of the einstein problem as a poc example. And improve its setup.
    There was one constraint that was missing that reduced the space and runtime. Also the implications (`a->b`) were encoded as `lte` but should be encoded as a biconditional (iif) `a<=>b`, which is an `eq`. This improved runtime dramatically.
    
    =============================== Coverage summary ================
    Statements   : 93.2% ( 4564/4897 ), 6 ignored
    Branches     : 87.91% ( 1935/2201 ), 40 ignored
    Functions    : 95.05% ( 461/485 ), 6 ignored
    Lines        : 94.01% ( 3952/4204 )
    =================================================================
    
    File dist/finitedomain.dist.min.js created: 214.84 kB → 53.31 kB → 15.94 kB (gzip)

--- tests/specs/einstein.spec.js
+++ tests/specs/einstein.spec.js
@@ -37,10 +37,20 @@ Pet [birds | cats | dogs | fish | horses]
 
 import expect from '../fixtures/mocha_proxy.fixt';
 import Solver from '../../src/solver';
+import importer from '../../src/importer';
 
 describe('src/einstein.spec', function() {
 
-  it('puzzle', function() {
+  function solutionFor(solution, varNames) {
+    let result = {};
+    for (let i = 0; i < varNames.length; i++) {
+      let varName = varNames[i];
+      result[varName] = solution[varName];
+    }
+    return result;
+  }
+
+  it('puzzle with api', function() {
     let solver = new Solver();
 
     let H = [0, 1, 2, 3, 4];
@@ -110,7 +120,7 @@ describe('src/einstein.spec', function() {
       // "iif house[i]=x then house[i]=y"
       let ifThenHouse = (a, b, houseNumber) => _iif(hash[a] + houseNumber, a, hash[b] + houseNumber, b);
       // "iif A=v1 then B=v2"
-      let _iif = (ha, va, hb, vb) => solver.lte(solver.isEq(ha, va), solver.isEq(hb, vb));
+      let _iif = (ha, va, hb, vb) => solver.eq(solver.isEq(ha, va), solver.isEq(hb, vb));
       // "iif house=x then house+=delta=y" (+1 or -1 for either neighbor)
       let ifThenNeighbor = (a, b, delta) => _iif(hash[a] + i, a, hash[b] + (i + delta), b);
       // "iif house=x then either neighbor=y (not both)"
@@ -127,8 +137,14 @@ describe('src/einstein.spec', function() {
       ifThen(Englishman, red);                             // 1
       ifThen(Swede, dogs);                                 // 2
       ifThen(Dane, tea);                                   // 3
-      if (i < 4) ifThenNeighbor(green, white, +1);         // 4
-      else solver.neq('C4', green);                        // - (otherwise last house can be green and ignore rule 4)
+      if (i === 0) {
+        solver.neq('C0', white);                           // 4; if left-most house is white it cannot be to the right of a green house
+        ifThenNeighbor(green, white, +1);                  // 4
+      } else if (i < 4) {
+        ifThenNeighbor(green, white, +1);                  // 4
+      } else {
+        solver.neq('C4', green);                           // 4: if right-most house is green then it cannot be to the left of a white house
+      }
       ifThen(green, coffee);                               // 5
       ifThen(PallMall, birds);                             // 6
       ifThen(yellow, Dunhill);                             // 7
@@ -143,17 +159,8 @@ describe('src/einstein.spec', function() {
     });
 
     // we only care about solving the assignments of each property for each house (C0, P0, etc)
-    solver.solve({_debug: 0, log: 1, max: 5, vars: [].concat(Cn, Nn, Dn, Sn, Pn)});
+    solver.solve({_debug: 0, log: 1, max: 2, vars: [].concat(Cn, Nn, Dn, Sn, Pn)});
     expect(solver.solutions.length, 'solution count').to.eql(1); // only has one solution
-
-    function solutionFor(solution, varNames) {
-      let result = {};
-      for (let i = 0; i < varNames.length; i++) {
-        let varName = varNames[i];
-        result[varName] = solution[varName];
-      }
-      return result;
-    }
 
     /*
     from the website:
@@ -195,6 +202,260 @@ describe('src/einstein.spec', function() {
       N4: s[Swede],
       P4: s[dogs],
       S4: s[BlueMaster],
+    });
+  });
+
+  it('puzzle with dsl', function() {
+
+    const blue = 0;
+    const green = 1;
+    const red = 2;
+    const white = 3;
+    const yellow = 4;
+
+    const dane = 0;
+    const english = 1;
+    const german = 2;
+    const swede = 3;
+    const norwegian = 4;
+
+    const bier = 0;
+    const coffee = 1;
+    const milk = 2;
+    const tea = 3;
+    const water = 4;
+
+    const blend = 0;
+    const bluemaster = 1;
+    const dunhill = 2;
+    const pallmall = 3;
+    const prince = 4;
+
+    const birds = 0;
+    const cats = 1;
+    const dogs = 2;
+    const fish = 3;
+    const horses = 4;
+
+    let dsl = `
+      # constants
+      : blue [${blue} ${blue}]
+      : green [${green} ${green}]
+      : red [${red} ${red}]
+      : white [${white} ${white}]
+      : yellow [${yellow} ${yellow}]
+
+      : dane [${dane} ${dane}]
+      : english [${english} ${english}]
+      : german [${german} ${german}]
+      : swede [${swede} ${swede}]
+      : norwegian [${norwegian} ${norwegian}]
+
+      : bier [${bier} ${bier}]
+      : coffee [${coffee} ${coffee}]
+      : milk [${milk} ${milk}]
+      : tea [${tea} ${tea}]
+      : water [${water} ${water}]
+
+      : blend [${blend} ${blend}]
+      : bluemaster [${bluemaster} ${bluemaster}]
+      : dunhill [${dunhill} ${dunhill}]
+      : pallmall [${pallmall} ${pallmall}]
+      : prince [${prince} ${prince}]
+
+      : birds [${birds} ${birds}]
+      : cats [${cats} ${cats}]
+      : dogs [${dogs} ${dogs}]
+      : fish [${fish} ${fish}]
+      : horses [${horses} ${horses}]
+
+      # which house is assigned which option?
+      # C=color, N=nationality, D=drink, S=smokes, P=pet
+      : C0 [0 4] alias(house_color_of_first_house)
+      : C1 [0 4]
+      : C2 [0 4]
+      : C3 [0 4]
+      : C4 [0 4]
+      : N0 [0 4] alias(nationality_of_first_house)
+      : N1 [0 4]
+      : N2 [0 4]
+      : N3 [0 4]
+      : N4 [0 4]
+      : D0 [0 4] alias(drink_of_first_house)
+      : D1 [0 4]
+      : D2 [0 4]
+      : D3 [0 4]
+      : D4 [0 4]
+      : S0 [0 4] alias(smoke_brand_of_first_house)
+      : S1 [0 4]
+      : S2 [0 4]
+      : S3 [0 4]
+      : S4 [0 4]
+      : P0 [0 4] alias(pet_of_first_house)
+      : P1 [0 4]
+      : P2 [0 4]
+      : P3 [0 4]
+      : P4 [0 4]
+
+      # distribution must be unique (one color per house, etc)
+      distinct(C0 C1 C2 C3 C4)
+      distinct(N0 N1 N2 N3 N4)
+      distinct(D0 D1 D2 D3 D4)
+      distinct(S0 S1 S2 S3 S4)
+      distinct(P0 P1 P2 P3 P4)
+
+      # now follow the encoding for each condition
+      # it's a bit repetitive because most of the time
+      # each house must get the same condition ("for all
+      # x; if color of house x is red then the pet of
+      # house x is cat", which would still apply to only
+      # one since the colors (and pets) of all houses
+      # are distinct from one another)
+
+      # 1: The Englishman lives in the red house
+      # "(Nx == english) <=> (Cx == red)"
+      (N0 ==? english) == (C0 ==? red)
+      (N1 ==? english) == (C1 ==? red)
+      (N2 ==? english) == (C2 ==? red)
+      (N3 ==? english) == (C3 ==? red)
+      (N4 ==? english) == (C4 ==? red)
+
+      # 2: The Swede keeps dogs
+      # similar to above
+      (N0 ==? swede) == (P0 ==? dogs)
+      (N1 ==? swede) == (P1 ==? dogs)
+      (N2 ==? swede) == (P2 ==? dogs)
+      (N3 ==? swede) == (P3 ==? dogs)
+      (N4 ==? swede) == (P4 ==? dogs)
+
+      # 3: The Dane drinks tea
+      (N0 ==? dane) == (D0 ==? tea)
+      (N1 ==? dane) == (D1 ==? tea)
+      (N2 ==? dane) == (D2 ==? tea)
+      (N3 ==? dane) == (D3 ==? tea)
+      (N4 ==? dane) == (D4 ==? tea)
+
+      # 4: the green house is just to the left of the white one
+      C0 != white
+      (C0 ==? green) == (C1 ==? white)
+      (C1 ==? green) == (C2 ==? white)
+      (C2 ==? green) == (C3 ==? white)
+      (C3 ==? green) == (C4 ==? white)
+      C4 != green
+
+      # 5: the owner of the green house drinks coffee
+      (C0 ==? green) == (D0 ==? coffee)
+      (C1 ==? green) == (D1 ==? coffee)
+      (C2 ==? green) == (D2 ==? coffee)
+      (C3 ==? green) == (D3 ==? coffee)
+      (C4 ==? green) == (D4 ==? coffee)
+
+      # 6: the pallmall smoker keeps birds
+      (S0 ==? pallmall) == (P0 ==? birds)
+      (S1 ==? pallmall) == (P1 ==? birds)
+      (S2 ==? pallmall) == (P2 ==? birds)
+      (S3 ==? pallmall) == (P3 ==? birds)
+      (S4 ==? pallmall) == (P4 ==? birds)
+
+      # 7: the owner of the yellow house smokes dunhills
+      (S0 ==? dunhill) == (C0 ==? yellow)
+      (S1 ==? dunhill) == (C1 ==? yellow)
+      (S2 ==? dunhill) == (C2 ==? yellow)
+      (S3 ==? dunhill) == (C3 ==? yellow)
+      (S4 ==? dunhill) == (C4 ==? yellow)
+
+      # 8: the man in the center house drinks milk
+      D2 == milk
+
+      # 9: the norwegian lives in the first house
+      N0 == norwegian
+
+      # 10: the blend smoker has a neighbor who keeps cats
+      # for the middle ones; if this house smokes blend, either the left or the right is cats but not both and not neither.
+      # that's why the neq works. we'd need an "or" for this case. exactly one of these five cases will force a Px set to cats.
+      (S0 ==? blend) == (P1 ==? cats)
+      (S1 ==? blend) <= ((P0 ==? cats) !=? (P2 ==? cats))
+      (S2 ==? blend) <= ((P1 ==? cats) !=? (P3 ==? cats))
+      (S3 ==? blend) <= ((P2 ==? cats) !=? (P4 ==? cats))
+      (S4 ==? blend) == (P3 ==? cats)
+
+      # 11: the man who smokes blue masters drinks bier
+      (S0 ==? bluemaster) == (D0 ==? bier)
+      (S1 ==? bluemaster) == (D1 ==? bier)
+      (S2 ==? bluemaster) == (D2 ==? bier)
+      (S3 ==? bluemaster) == (D3 ==? bier)
+      (S4 ==? bluemaster) == (D4 ==? bier)
+
+      # 12: the man who keeps horses lives next to the dunhill smoker
+      # (see 10)
+      (P0 ==? horses) == (S1 ==? dunhill)
+      (P1 ==? horses) <= ((S0 ==? dunhill) !=? (S2 ==? dunhill))
+      (P2 ==? horses) <= ((S1 ==? dunhill) !=? (S3 ==? dunhill))
+      (P3 ==? horses) <= ((S2 ==? dunhill) !=? (S4 ==? dunhill))
+      (P4 ==? horses) == (S3 ==? dunhill)
+
+      # 13: the german smokes prince
+      (N0 ==? german) == (S0 ==? prince)
+      (N1 ==? german) == (S1 ==? prince)
+      (N2 ==? german) == (S2 ==? prince)
+      (N3 ==? german) == (S3 ==? prince)
+      (N4 ==? german) == (S4 ==? prince)
+
+      # 14: the norwegian lives next to the blue house
+      # (see 10)
+      (N0 ==? norwegian) == (C1 ==? blue)
+      (N1 ==? norwegian) <= ((C0 ==? blue) !=? (C2 ==? blue))
+      (N2 ==? norwegian) <= ((C1 ==? blue) !=? (C3 ==? blue))
+      (N3 ==? norwegian) <= ((C2 ==? blue) !=? (C4 ==? blue))
+      (N4 ==? norwegian) == (C3 ==? blue)
+
+      # 15: the blend smoker has a neighbor who drinks water
+      # note: this is the same pre-condition as 10. we could use "and" for this kind of case. the dupe should be optimized away.
+      (S0 ==? blend) == (D1 ==? water)
+      (S1 ==? blend) <= ((D0 ==? water) !=? (D2 ==? water))
+      (S2 ==? blend) <= ((D1 ==? water) !=? (D3 ==? water))
+      (S3 ==? blend) <= ((D2 ==? water) !=? (D4 ==? water))
+      (S4 ==? blend) == (D3 ==? water)
+    `;
+
+    let solver = importer(dsl);
+
+    let outputVars = ['C0', 'C1', 'C2', 'C3', 'C4', 'N0', 'N1', 'N2', 'N3', 'N4', 'D0', 'D1', 'D2', 'D3', 'D4', 'S0', 'S1', 'S2', 'S3', 'S4', 'P0', 'P1', 'P2', 'P3', 'P4'];
+    solver.solve({log: 1, max: 2, vars: outputVars});
+
+    expect(solver.solutions.length, 'solution count').to.eql(1);
+
+    let s = solver.solutions[0];
+    expect(solutionFor(s, outputVars)).to.eql({
+      C0: yellow,
+      D0: water,
+      N0: norwegian,
+      P0: cats,
+      S0: dunhill,
+
+      C1: blue,
+      D1: tea,
+      N1: dane,
+      P1: horses,
+      S1: blend,
+
+      C2: red,
+      D2: milk,
+      N2: english,
+      P2: birds,
+      S2: pallmall,
+
+      C3: green,
+      D3: coffee,
+      N3: german,
+      P3: fish, // <-- ! :)
+      S3: prince,
+
+      C4: white,
+      D4: bier,
+      N4: swede,
+      P4: dogs,
+      S4: bluemaster,
     });
   });
 });

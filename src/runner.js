45b52dd6e3ef24aecc8be450bdff1f8fd33a3530
Peter van der Zee <github-public@qfox.nl>
2017-02-17 11:29:17 +0100

    Move problem functions to its own file
    =============================== Coverage summary ================
    Statements   : 75.05% ( 7079/9433 ), 6 ignored
    Branches     : 64.02% ( 2829/4419 ), 23 ignored
    Functions    : 77.86% ( 443/569 ), 3 ignored
    Lines        : 76.63% ( 6399/8351 )
    =================================================================
    
    File dist/finitedomain.dist.min.js created: 408.38 kB → 98.02 kB → 27.25 kB (gzip)

--- src/runner.js
+++ src/runner.js
@@ -10,13 +10,10 @@ import {
   $REJECTED,
   $SOLVED,
   $STABLE,
-  SUB,
-  SUP,
 
   ASSERT,
   ASSERT_LOG2,
   THROW,
-  TRACE_ADD,
 } from './helpers';
 import {
   ml_countConstraints,
@@ -40,17 +37,9 @@ import {
 } from './cutter';
 import {
   domain__debug,
-  domain_createRange,
-  domain_createValue,
   domain_getValue,
-  domain_min,
-  domain_arrToSmallest,
   domain_toArr,
 } from './domain';
-import {
-  trie_add,
-  trie_get,
-} from './trie';
 import {
   counter,
 } from './counter';
@@ -67,73 +56,14 @@ function solverSolver(dsl) {
 
   let problem = problem_create(dsl);
   // you can destructure it but this way is much easier to grep for usages... let the minifier minify it
-  let varTrie = problem.varTrie;
   let varNames = problem.varNames;
-  let aliases = problem.aliases; // map old index to new index
   let domains = problem.domains;
   let solveStack = problem.solveStack;
 
-  let anonCounter = 0;
-
-  function $addVar(name, domain, modifier, returnName, returnIndex) {
-    ASSERT_LOG2('addVar', name, domain, modifier, returnName ? '(return name)' : '', returnIndex ? '(return index)' : '');
-    if (modifier) THROW('implement me (var mod)');
-    if (typeof name === 'number') {
-      domain = name;
-      name = undefined;
-    }
-    if (name === undefined) {
-      name = '__' + (++anonCounter);
-      ASSERT_LOG2(' - Adding anonymous var for dom=', domain, '->', name);
-    }
-    if (typeof domain === 'number') {
-      domain = domain_createValue(domain);
-    } else if (domain === undefined) {
-      domain = domain_createRange(SUB, SUP);
-    } else {
-      domain = domain_arrToSmallest(domain);
-    }
-
-    let newIndex = varNames.length;
-    trie_add(varTrie, name, newIndex);
-    varNames.push(name);
-    domains.push(domain);
-    if (returnIndex) return newIndex;
-    if (returnName) return name; // return a name when explicitly asked for.
-  }
-
-  function $getVar(name) {
-    return trie_get(varTrie, name);
-  }
-
-  function $addAlias(indexOld, indexNew) {
-    ASSERT(indexOld !== indexNew, 'cant make an alias for itself');
-    aliases[indexOld] = indexNew;
-
-    ASSERT_LOG2(' - Mapping', indexOld, 'to be an alias for', indexNew);
-    TRACE_ADD(indexOld, domain__debug(domains[indexOld]), domain__debug(domains[indexNew]), 'now alias of ' + indexNew);
-    solveStack.push(domains => {
-      ASSERT_LOG2(' - alias; ensuring', indexNew, 'and', indexOld, 'result in same value');
-      ASSERT_LOG2('   - domain =', domain__debug(domains[indexNew]), 'forcing choice to min(d)=', domain_min(domains[indexNew]));
-      // ensure A and B end up with the same value, regardless of how A is reduced
-      ASSERT(domains[indexOld] === false, 'B should be marked as an alias');
-      domains[indexOld] = domains[indexNew] = domain_createValue(domain_min(domains[indexNew]));
-    });
-    ASSERT(!void (solveStack[solveStack.length - 1]._target = indexOld));
-    ASSERT(!void (solveStack[solveStack.length - 1]._meta = 'alias(' + indexNew + ' -> ' + indexOld + ')'));
-    domains[indexOld] = false; // mark as aliased. this is not a change per se.
-  }
-
-  function $getAlias(index) {
-    let alias = aliases[index];
-    if (alias === index) throw new Error('alias is itself?', alias, index);
-    if (alias === undefined) {
-      console.log(JSON.stringify(domains));
-      console.log(JSON.stringify(aliases).replace(/"/g, ''));
-      throw new Error('alias for ' + index + ' does not exist... ');
-    }
-    return alias;
-  }
+  let $addVar = problem.addVar;
+  let $getVar = problem.getVar;
+  let $addAlias = problem.addAlias;
+  let $getAlias = problem.getAlias;
 
   ASSERT_LOG2('Parsing DSL...');
   console.time('- dsl->ml (' + dsl.length + ')');
